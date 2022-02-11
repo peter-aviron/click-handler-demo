b4a29813b6040a174107681eae8b1ba1f495f49e
Peter Mayor <peter@avironsoftware.com>
2022-02-11 17:55:02 -0500

    init

--- assets/javascript/clickhandler.js
+++ assets/javascript/clickhandler.js
@@ -2,31 +2,29 @@ let colorBoxElement = document.querySelector("#color-box");
 
 // JS for the gray button
 let grayButton = document.querySelector("#gray-button");
+
 grayButton.addEventListener("click", function () {
     colorBoxElement.className = "gray";
 })
 
 // JS for the blue button
 let blueButton = document.querySelector("#blue-button");
+
 blueButton.addEventListener("click", function () {
     colorBoxElement.className = "blue";
 })
 
 // JS for the pink button
-let pinkButton = document.querySelector("#pink-button");
-pinkButton.addEventListener("click", function () {
-    colorBoxElement.className = "pink";
-})
 
 let count = 0;
 let countspanElement = document.querySelector("#countspan");
 let counterButton = document.querySelector("#counter-button");
 counterButton.addEventListener("click", function () {
     // assign the value of count to be the old count value plus 1
-    count = count + 1
+    
     
     // assign the innerHTML of countspanElement to count
-    countspanElement.innerHTML = count
+    
 })
 let evenOrOddButton = document.querySelector("#even-or-odd-button");
 evenOrOddButton.addEventListener("click", function () {
